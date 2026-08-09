83af6f44fbc14e414e79c47e810efeef0c584233
chanduzumba <chanduzumba@gmail.com>
2026-08-09 18:39:48 +0530

    text splitters over document array to get chunks

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 import { Document } from "@langchain/core/documents";
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
 
 async function loadTextFile(): Promise<void> {
   const loader = new PDFLoader("./docs/US_Mortgage_Lifecycle_Guide.pdf");
@@ -11,11 +12,22 @@ async function loadTextFile(): Promise<void> {
     console.log("Content:", doc.pageContent.substring(0,100));
     console.log("Metadata:", doc.metadata.source);
   });
-  console.log(docs.length)
-  console.log(typeof docs[0].pageContent);
-console.log(docs[0].pageContent.length);
-console.log(Object.keys(docs[0].metadata));
-console.log(docs[1].metadata.pdf);
-console.log(docs[1].metadata.loc);
+//   console.log(docs.length)
+//   console.log(typeof docs[0].pageContent);
+// console.log(docs[0].pageContent.length);
+// console.log(Object.keys(docs[0].metadata));
+// console.log(docs[1].metadata.pdf);
+// console.log(docs[1].metadata.loc);
+
+const splitter = new RecursiveCharacterTextSplitter({ chunkSize: 1000, chunkOverlap: 200 })
+const chunks = await splitter.splitDocuments(docs)
+
+console.log(chunks[0].pageContent.length);
+
+console.log(chunks[1].pageContent.length);
+
+console.log(chunks[0].metadata);
+
+console.log(chunks[1].metadata);
 }
 await loadTextFile()
